230d1364d243e9a687dad9fbb077358e47d7c616
AndriiLata <lataandrii@gmail.com>
2024-12-27 14:08:52 +0100

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-base-200">
+      <button className="btn btn-error">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
